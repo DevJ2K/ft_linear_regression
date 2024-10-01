c83c96b338a1879444861d67b24354ce6a1bec26
DevJ2K <ajavontheopro@gmail.com>
2024-10-01 16:41:16 +0200

    ✅ : End of my first machine learning algorithm with explained

--- learn_how_to_train_model.py
+++ learn_how_to_train_model.py
@@ -2,6 +2,8 @@ import csv
 import numpy as np
 # m: nombre d'exemples
 # n: nombre de features
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
 
 dataset = []
 
@@ -12,12 +14,69 @@ with open('data.csv') as csvfile: #Dimensions = 24 * 2
 	# for row in reader:
 	# 	print(row['km'], row['price'])
 
+# def estimatePrice(mileage: int, theta0: int = 0, theta1: int = 0) -> int:
+# 	return theta0 + (theta1 * mileage)
 
-def estimatePrice(mileage: int, theta0: int = 0, theta1: int = 0) -> int:
-	return theta0 + (theta1 * mileage)
+# MODELE
+def model(X: np.ndarray, theta: np.ndarray):
+	return X.dot(theta)
 
-print(dataset)
-print(dataset.shape)
+# FUNCTION COUT
+def cost_function(X, y, theta):
+	m = len(y)
+	return 1/(2 * m) * np.sum((model(X, theta) - y) ** 2)
+
+# GRADIENT
+def grad(X, y, theta):
+	m = len(y)
+	return 1/m * X.T.dot(model(X, theta) - y)
+
+# DESCENTE DE GRADIENT
+def gradient_descent(X, y, theta, learning_rate, n_iterations: int):
+	cost_history = np.zeros(n_iterations)
+	for i in range(0, n_iterations):
+		theta = theta - learning_rate * grad(X, y, theta)
+		cost_history[i] = cost_function(X, y, theta)
+	return theta, cost_history
+
+def coef_determination(y, pred):
+	u = ((y - pred) ** 2).sum()
+	v = ((y - y.mean()) ** 2).sum()
+	return (1 - u/v)
+
+x, y = make_regression(n_samples=100, n_features=1, noise=10)
+y = y.reshape(y.shape[0], 1)
+plt.scatter(x, y)
+
+print(x.shape)
+print(y.shape)
+
+# Matrice X
+X = np.hstack((x, np.ones(x.shape)))
+print(X)
+
+theta = np.random.randn(2, 1)
+print(theta.shape)
+print(theta)
+
+print(cost_function(X, y, theta))
+print(theta)
+theta_final, cost_history = gradient_descent(X, y, theta, learning_rate=0.1, n_iterations=100)
+print(cost_function(X, y, theta_final))
+print(theta_final)
+
+predictions = model(X, theta_final)
+
+print(coef_determination(y, predictions))
+# plt.plot(x, predictions, c='r')
+
+# plt.plot(x, model(X=X, theta=theta), c='g')
+
+plt.plot(range(100), cost_history)
+
+plt.show()
+# print(dataset)
+# print(dataset.shape)
 
 # Modèles : f(x) = ax + b
 # Paramètres : a, b
