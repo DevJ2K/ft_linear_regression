46e73c48d6c63ce055f14ee62c4593e17d63cc47
DevJ2K <ajavontheopro@gmail.com>
2024-10-23 17:12:40 +0200

    🏆 : Graph can be animate or not !

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,74 +1,23 @@
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from Normalization import Normalization
 
 class DataError(Exception):
 	pass
 
 class LinearRegression:
-	# figure, axis = plt.subplots(2, 2)
 
-	# # For Sine Function
-	# axis[0, 0].plot(X, Y1)
-	# axis[0, 0].set_title("Sine Function")
-
-	# # For Cosine Function
-	# axis[0, 1].plot(X, Y2)
-	# axis[0, 1].set_title("Cosine Function")
-
-	# # For Tangent Function
-	# axis[1, 0].plot(X, Y3)
-	# axis[1, 0].set_title("Tangent Function")
-
-	# # For Tanh Function
-	# axis[1, 1].plot(X, Y4)
-	# axis[1, 1].set_title("Tanh Function")
-
-	def __init__(self, file: str):
-
-		# np.set_printoptions(suppress=True)
-		# Matplotlib
-		# self.fig, self.axis = plt.subplots(2, 2)
-		self.fig, self.axis = plt.subplots(3, 1)
-		self.fig.canvas.manager.set_window_title("Linear Regression")
-
-		self.cost_history = []
-
-		# Init Data
-		try:
-			with open(file) as csvfile: #Dimensions = 24 * 2
-				reader = csv.DictReader(csvfile)
-				self.data = np.array([(int(row['km']), int(row['price'])) for row in reader])
-		except:
-			raise DataError(f"Failed to initialize data from provide file '{file}'.")
-
-		# print(self.data)
-		self.x_data = self.data[:,0].reshape(-1, 1)
-		self.y_data = self.data[:,1].reshape(-1, 1)
-
-		self.normalize_x_data = Normalization(self.x_data)
-		self.normalize_y_data = Normalization(self.y_data)
-
-		self.x_standardized = self.normalize_x_data.standardize_all()
-		self.y_standardized = self.normalize_y_data.standardize_all()
-
-		self.x_data = self.x_standardized
-		self.y_data = self.y_standardized
-
-		self.X = np.hstack((self.x_standardized, np.ones((self.x_data.shape[0], 1))))
-
-		# self.X = np.hstack((self.x_data, np.ones((self.x_data.shape[0], 1))))
-
-		# self.theta = np.array([-0.025, 9000]).reshape(2, 1)
-		self.theta = np.zeros((2, 1))
-
-		# print(self.data)
-		# print(self.data.shape)
-		# for x, y in zip(self.x, self.y):
-		# 	print(f"f({x}) = {y}")
-
-		# self.__plot_data()
+	def __init__(self):
+		self.plot_info = {
+			'window_title': 'Linear Regression',
+			'window_bg': '#d1d2ff',
+			'data_color': '#0c0066',
+			'model_color': '#eb7eff',
+			'font_axis': {'color':'#252525'},
+			'font_title': {'color':'#000000'}
+		}
 
 
 	def __model(self, X: np.ndarray, theta: np.ndarray) -> np.ndarray:
@@ -77,29 +26,10 @@ class LinearRegression:
 		Returns:
 			np.ndarray: Shape (m x 1)
 		"""
-		# theta = np.array([-0.004, 10.]).reshape(2, 1)
-		# np.set_printoptions(suppress=True)
-		# print(X)
-		# print(y)
-		# print(theta)
-		# print("X dot Theta")
-		# for y_real, y_pred in zip(y, X.dot(theta)):
-		# 	print(f"{y_real} - {y_pred}")
-		# # print(X.dot(theta))
-		# print(X.dot(theta).shape)
-
 		return X.dot(theta)
 
 	def __cost_function(self, X: np.ndarray, y: np.ndarray, theta: np.ndarray) -> float:
-		# theta = np.array([-0.004, 10.]).reshape(2, 1)
 		m = X.shape[0]
-		# print("MODEL PRED :")
-		# print(self.__model(X, theta))
-		# print("Y")
-		# print(y)
-		# print("DIFF")
-		# print(np.sum((self.__model(X, theta) - y) ** 2))
-		# print(1/(2 * m) * np.sum((self.__model(X, theta) - y) ** 2))
 		return 1/(2 * m) * np.sum((self.__model(X, theta) - y) ** 2)
 
 	def __gradient(self, X: np.ndarray, y: np.ndarray, theta: np.ndarray) -> np.ndarray:
@@ -122,20 +52,89 @@ class LinearRegression:
 		self.theta = theta
 
 
+	def update(self, i):
+		plot_info = self.plot_info
+
+		self.theta = self.theta - self.learning_rate * self.__gradient(self.X, self.y_standardized, self.theta)
+		self.cost_history[i] = self.__cost_function(self.X, self.y_standardized, self.theta)
+		self.coef_determination_history[i] = self.__coef_determination(self.y_standardized, self.__model(self.X, self.theta))
+
+		self.axis_model.clear()
+
+		self.axis_model.scatter(self.x_data, self.y_data, c=plot_info['data_color'])
+		self.axis_model.set_ylim([self.y_data.min() - 200, self.y_data.max() + 200])
+
+		self.axis_model.plot(self.x_data, self.normalize_y_data.destandardize(self.__model(self.X, self.theta)), c=plot_info['model_color'])
+
+		self.axis_model.set_xlabel("Mileage (km)", fontdict=plot_info['font_axis'])
+		self.axis_model.set_ylabel("Price (€)", fontdict=plot_info['font_axis'])
+		self.axis_model.legend(['Training Data','Model Prediction'], loc='upper right')
+		self.axis_model.set_title("Model Prediction vs Training Data", fontdict=plot_info['font_title'])
+
+
+
+	def __animate_gradient_descent(self, n_iteration: int):
+		self.fig, self.axis_model = plt.subplots(1, 1)
+		self.fig.canvas.manager.set_window_title(self.plot_info['window_title'])
+		self.fig.set_facecolor(self.plot_info['window_bg'])
+
+		self.cost_history = np.zeros(n_iteration)
+		self.coef_determination_history = np.zeros(n_iteration)
+
+		anim = FuncAnimation(self.fig, self.update, frames=np.arange(1, n_iteration), interval=0)
+		plt.show()
+
 	def __coef_determination(self, y: np.ndarray, pred: np.ndarray):
+		"""
+		To calculate the precision of the model.
+		"""
 		u = ((y - pred) ** 2).sum()
 		v = ((y - y.mean()) ** 2).sum()
 		return (1 - u/v)
 
 
-	def train_model(self, iterations: int = 1000, learningRate: float = 0.005):
-		self.__gradient_descent(self.X, self.y_data, self.theta, iterations, learningRate)
-		self.__plot_data(iterations=iterations)
+	def train_model(self,
+				file: str,
+				iterations: int = 1000,
+			learningRate: float = 0.01,
+			animate: bool = False
+			):
 
-		print(self.__coef_determination(self.y_data, self.__model(self.X, self.theta)))
-		plt.show()
+		try:
+			with open(file) as csvfile:
+				reader = csv.DictReader(csvfile)
+				self.data = np.array([(float(row['km']), float(row['price'])) for row in reader])
+		except:
+			raise DataError(f"Failed to initialize data from provide file '{file}'.")
 
-	def use_model(self, mileage: int) -> float:
+		self.learning_rate = learningRate
+
+		# We don't standardise this data.
+		self.x_data = self.data[:,0].reshape(-1, 1)
+		self.y_data = self.data[:,1].reshape(-1, 1)
+
+
+		self.normalize_x_data = Normalization(self.x_data)
+		self.normalize_y_data = Normalization(self.y_data)
+
+		# Data standardized
+		self.x_standardized = self.normalize_x_data.standardize_all()
+		self.y_standardized = self.normalize_y_data.standardize_all()
+
+
+		# We use a standardized X matrice to train the model.
+		self.X = np.hstack((self.x_standardized, np.ones((self.x_data.shape[0], 1))))
+		self.theta = np.zeros((2, 1))
+
+
+		# print(self.__coef_determination(self.y_data, self.__model(self.X, self.theta)))
+		if (animate == True):
+			self.__animate_gradient_descent(iterations)
+		else:
+			self.__gradient_descent(self.X, self.y_standardized, self.theta, iterations, learningRate)
+			self.__plot_data(iterations=iterations)
+
+	def use_model(self, mileage) -> float:
 		if (mileage < 0):
 			raise DataError("Mileage cannot be less than 0.")
 		print("PREDICTION")
@@ -153,72 +152,75 @@ class LinearRegression:
 		# print(self.normalize_y_data.destandardize(pred))
 		# print(self.__model([240000], self.theta))
 		# LOAD LE MODEL
-		pass
+
 
 
 	def __plot_data(self, iterations: int):
 
+		plot_info = self.plot_info
+		self.fig, self.axis = plt.subplots(2, 2)
+		self.fig.canvas.manager.set_window_title(plot_info['window_title'])
+
+		self.axis_model = self.axis[0][0]
+		self.axis_diff_cost_precision = self.axis[0][1]
+		self.axis_cost = self.axis[1][0]
+		self.axis_precision = self.axis[1][1]
+
+
 		# https://matplotlib.org/stable/users/explain/animations/animations.html
 		# Implement curses animation
 
-		# font_axis = {'family':'poppins','color':'#000494','size':12}
-		font_axis = {'color':'#000494','size':12}
+		# self.fig.canvas.manager.set_window_title("Linear Regression")
 
-		(axis_model, axis_cost, axis_precision) = self.axis
+		font_axis = {'color':'#252525'}#,'size':12}
+		font_title = {'color':'#000000'}#,'size':12}
 
+		# (axis_model, axis_diff_cost_precision, axis_cost, axis_precision) = self.axis
+		# axis_model = self.axis[0][0]
+		# axis_diff_cost_precision = self.axis[0][1]
+		# axis_cost = self.axis[1][0]
+		# axis_precision = self.axis[1][1]
 
-		axis_cost.plot(range(iterations), self.cost_history)
-		# axis_cost.set_xlabel("Mileage (km)", fontdict=font_axis)
-		# axis_cost.set_ylabel("Price (€)", fontdict=font_axis)
+		self.fig.set_facecolor(plot_info['window_bg'])
 
+		self.axis_model.scatter( self.x_data, self.y_data, c=plot_info['data_color'])
 
-		# self.theta = self.theta * (self.normalize_y_data.standard_deviation / self.normalize_x_data.standard_deviation)
-		# self.theta[0] = self.theta * (self.normalize_y_data.standard_deviation / self.normalize_x_data.standard_deviation)
-		# self.theta[1] = self.normalize_y_data.mean - np.sum(self.normalize_x_data.mean * self.theta)
-		# print(self.theta)
-		# self.x_data = self.normalize_x_data.destandardize_all()
-		# self.y_data = self.normalize_y_data.destandardize_all()
-
-		# self.X = np.hstack((self.x_data, np.ones((self.x_data.shape[0], 1))))
-
-		# axis_model.scatter(self.x_data, self.y_data)
-		# axis_model.plot(self.x_data, self.__model(self.X, self.theta), c='r')
-		# axis_model.set_xlabel("Mileage (km)", fontdict=font_axis)
-		# axis_model.set_ylabel("Price (€)", fontdict=font_axis)
+		self.axis_model.plot(self.x_data, self.normalize_y_data.destandardize(self.__model(self.X, self.theta)), c=plot_info['model_color'])
+		self.axis_model.set_xlabel("Mileage (km)", fontdict=plot_info['font_axis'])
+		self.axis_model.set_ylabel("Price (€)", fontdict=plot_info['font_axis'])
+		self.axis_model.legend(['Training Data','Model Prediction'], loc='upper right')
+		self.axis_model.set_title("Model Prediction vs Training Data", fontdict=plot_info['font_title'])
 
 
-		axis_model.scatter( self.normalize_x_data.destandardize(self.x_data), self.normalize_y_data.destandardize(self.y_data))
 
-		axis_model.plot(self.normalize_x_data.destandardize(self.x_data), self.normalize_y_data.destandardize(self.__model(self.X, self.theta)), c='r')
-		axis_model.set_xlabel("Mileage (km)", fontdict=font_axis)
-		axis_model.set_ylabel("Price (€)", fontdict=font_axis)
+		self.axis_diff_cost_precision.plot(range(iterations), self.coef_determination_history, c='g')
+		self.axis_diff_cost_precision.plot(range(iterations), self.cost_history, c='r')
+		self.axis_diff_cost_precision.set_yticks([(i) for i in np.arange(0, 1.25, 0.25)])
+		self.axis_diff_cost_precision.legend(['Precision (R²)','Cost Function'], loc='upper right')
+		self.axis_diff_cost_precision.set_xlabel("Iterations", fontdict=plot_info['font_axis'])
+		self.axis_diff_cost_precision.axes.get_yaxis().set_visible(False)
+		self.axis_diff_cost_precision.set_title("Cost Function vs Precision Over Iterations", fontdict=plot_info['font_title'])
 
+		self.axis_cost.plot(range(iterations), self.cost_history, c='r')
+		self.axis_cost.legend(['Cost Function'], loc='upper right')
+		self.axis_cost.set_xlabel("Iterations", fontdict=plot_info['font_axis'])
+		self.axis_cost.set_ylabel("Cost", fontdict=plot_info['font_axis'])
+		self.axis_cost.set_title("Cost Function Evolution Over Iterations", fontdict=plot_info['font_title'])
 
-		axis_precision.plot(range(iterations), self.coef_determination_history)
-		# plt.scatter(self.x_data, )
-		# default_theta = np.zeros((2, 1))
-		# axis_model.plot(self.x_data, self.__model(self.X, default_theta), c='y')
-
-		# self.axis[1].legend([None, 'First List', 'Second List'], loc='upper left')
-
-		# plt.plot(self.x_data, self.__model(self.X, self.y_data, self.theta), c='r')
-		# plt.tick_params(axis='x', which='major', labelsize=9)
-		# plt.xticks(self.x_data, [str(i) for i in self.x_data], rotation=70)
+		self.axis_precision.plot(range(iterations), self.coef_determination_history * 100, c='g')
+		self.axis_precision.set_yticks([i for i in np.arange(0, 125, 25)])
+		self.axis_precision.legend(['Precision (R² in %)'], loc='upper right')
+		self.axis_precision.set_ylabel("Precision (%)", fontdict=plot_info['font_axis'])
+		self.axis_precision.set_xlabel("Iterations", fontdict=plot_info['font_axis'])
+		self.axis_precision.set_title("Model Precision Evolution (R²) Over Iterations", fontdict=plot_info['font_title'])
 
 		plt.tight_layout()
-		# self.axis[0, 0].set_title("Data")
-
-
-	# def __show(self):
-	# 	self.__plot_data()
-	# 	# print(self.__coef_determination(self.y_data, self.__model(self.X, self.theta)))
-	# 	# print(self.theta)
-	# 	plt.show()
+		plt.show()
 
 if __name__ == "__main__":
 	try:
-		linearRegression = LinearRegression("data.csv")
-		linearRegression.train_model(iterations=1500, learningRate=0.005)
+		linearRegression = LinearRegression()
+		linearRegression.train_model(file="data.csv", iterations=1000, learningRate=0.01, animate=True)
 		# linearRegression.use_model(int(input("Entrez votre kilometrage")))
 		# linearRegression.show()
 	except Exception as e:
